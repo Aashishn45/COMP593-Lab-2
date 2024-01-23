df5ca78ff494675593395d4666ecb7980e303a00
Aashish Neupane <156460188+Aashishn45@users.noreply.github.com>
2024-01-23 11:42:50 -0500

    Lab-2.py
    This is lab 2.

---
+++ lab2_script_template(2).py
@@ -0,0 +1,76 @@
+def main():
+
+    # TODO: Step 2 - Create a complex data structure
+    about_me = {
+        "full_name": "Aashish Neupane",
+        "student_id": 10316514,
+        "pizza_toppings": ["SAUSAGE", "CABBAGE", "BACON"],
+        "movies": [
+           {
+               "title": "indiana jones",
+               "genre": "mystery",
+           },
+           
+           {
+               "title": "titanic",
+               "genre": "romance",
+           }, 
+        ],
+
+    }
+
+    # TODO: Step 3 - Add another movie to the data structure
+    about_me["movies"].append({"title": "avengers", "genre": "sc-fi"})
+
+    print_student_name_and_id(about_me)
+    add_pizza_toppings(about_me, toppings=("tomato", "chicken"))
+
+# TODO: Step 4 - Function that prints student name and ID	
+def print_student_name_and_id(about_me):
+    full_name = about_me["full_name"]
+    first_name = full_name.split()[0]
+    student_id = about_me["student_id"]
+    print(f'My name is {full_name}, but you can call me Sir {first_name}.\nMy student ID is {student_id}.')
+    return
+    
+# TODO: Step 5 - Function that adds pizza toppings to data structure
+def add_pizza_toppings(about_me, toppings):
+    for topp in toppings:
+        about_me["pizza_toppings"].append(topp)
+   
+    about_me["pizza_toppings"].sort()
+    new_list=[]
+    for topping in about_me["pizza_toppings"]:
+        topping.lower()
+        new_list.append(topping)
+
+    about_me["pizza_toppings"] = new_list 
+
+    return
+
+
+# TODO: Step 6 - Function that prints bullet list of pizza toppings
+def print_pizza_toppings(about_me):
+    for topping in about_me["pizza_toppings"]:
+    print(f'My favorite pizza toppings are:
+          
+          
+          '
+
+
+
+
+    )
+    return
+
+
+# TODO: Step 7 - Function that prints comma-separated list of movie genres
+def print_movie_genres(about_me):
+    return 
+
+# TODO: Step 8 - Function that prints comma-separated list of movie titles
+def print_movie_titles(movie_list):
+    return
+    
+if __name__ == '__main__':
+    main()
